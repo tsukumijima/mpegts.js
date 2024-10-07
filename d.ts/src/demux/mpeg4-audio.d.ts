fb25f542b559e395fea6d6ebab9db403583eca60
tsukumi <tsukumijima@users.noreply.github.com>
2024-10-07 15:09:44 +0900

    dist: Update dist file to install npm package from git

--- d.ts/src/demux/mpeg4-audio.d.ts
+++ d.ts/src/demux/mpeg4-audio.d.ts
@@ -1,10 +1,10 @@
 export declare enum MPEG4AudioObjectTypes {
     kNull = 0,
     kAACMain = 1,
-    kAAC_LC = 2,
+    kAAC_LC = 2,// LC-AAC
     kAAC_SSR = 3,
     kAAC_LTP = 4,
-    kAAC_SBR = 5,
+    kAAC_SBR = 5,// HE-AAC
     kAAC_Scalable = 6,
     kLayer1 = 32,
     kLayer2 = 33,
